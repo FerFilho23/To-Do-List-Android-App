0445317ec3274357872a21a7dfe94dbd7e2e9299
Fernando Filho <fernando.rsf23@gmail.com>
2022-01-21 11:19:36 -0300

    loading dialog

--- app/src/main/java/com/codinginflow/mvvmtodo/view/MainActivity.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/view/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.codinginflow.mvvmtodo.view
 
 import android.app.Activity
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.codinginflow.mvvmtodo.R
+import com.codinginflow.mvvmtodo.util.LoadingDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,6 +22,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val loading = LoadingDialog(this)
+        loading.startLoading()
+        val handler = Handler()
+        handler.postDelayed(object :Runnable{
+            override fun run() {
+                loading.isDismiss()
+            }
+        }, 3000)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
